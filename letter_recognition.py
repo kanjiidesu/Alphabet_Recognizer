637cd7d2584237cb8e2c7381eff6ad63b5ee577e
karina.laursen <karina.laursen@ssi-schaefer.com>
2023-12-14 10:38:56 +0100

    something important

--- letter_recognition.py
+++ letter_recognition.py
@@ -18,7 +18,6 @@ from keras.models import load_model
 from keras import Sequential, Model
 from keras.src.utils import to_categorical
 from keras.src.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
-# from sklearn.metrics import confusion_matrix
 
 MODEL_FILENAME: str = "letter_recognizer.h5"
 CHARACTER_COUNT = 26
@@ -178,6 +177,7 @@ def predict_letter(image: str, model: Model | str = MODEL_FILENAME, show_convert
 #
 #     # Create confusion matrix
 #     # TODO Kevin: Maybe fix plz IDK
+#     # TODO Kevin: DELETE if not used
 #     cm = confusion_matrix(y_true_labels, y_pred_labels)
 #
 #     # Plot confusion matrix
